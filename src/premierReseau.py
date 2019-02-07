4451a4f5b87e2799064bdd014cb488c43f32f598
Aurélien Delage <aurelien.delage@telecomnancy.eu>
2019-02-07 17:06:40 +0100

    lol

--- src/premierReseau.py
+++ src/premierReseau.py
@@ -10,6 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from scipy.stats import pearsonr
 tf.InteractiveSession()
 
 """
@@ -19,6 +20,10 @@ a = tf.zeros((2,4))
 randMat = tf.random_uniform((2,3), 0, 1)
 print(randMat.eval())
 """
+
+def pearson_r2_score(y, y_pred):
+  """Computes Pearson R^2 (square of Pearson correlation)."""
+  return pearsonr(y, y_pred)[0]**2
 
 #création d'un jeu de données de N points autour de la droite affine y=ax+b
 N = 100
@@ -36,7 +41,7 @@ plt.ylabel("y")
 plt.xlim(0, 1)
 plt.title("Toy Linear Regression Data, "r"$y = 5x + 2 + N(0, 1)$")
 plt.show()
-"""
+
 #Génération d'un graphe TensorFlow
 with tf.name_scope("placeholders"): #données d'entrée
   x = tf.placeholder(tf.float32, (N, 1))
@@ -49,26 +54,34 @@ with tf.name_scope("prediction"): #y calculés avec les poids du graphe
 with tf.name_scope("loss"): #fonction de perte
   l = tf.reduce_sum((y - tf.squeeze(y_pred))**2)
 with tf.name_scope("optim"): #fonction d'optimisation
-	train_op = tf.train.AdamOptimizer(.001).minimize(l)
+	train_op = tf.train.AdamOptimizer(.1).minimize(l)
 
 
 with tf.name_scope("summaries"): #pour sauvegarder l'évolution de la fonction de perte
   tf.summary.scalar("loss", l)
   merged = tf.summary.merge_all()
 
-train_writer = tf.summary.FileWriter('/tmp/lr-train', tf.get_default_graph())
+train_writer = tf.summary.FileWriter('/tmp/lr2-train', tf.get_default_graph())
 
-n_steps = 20000 #nombre d'entrainements
+n_steps = 2000 #nombre d'entrainements
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   #entraiement du réseau
   for i in range(n_steps):
     feed_dict = {x: x_np, y: y_np}
     _, summary, loss = sess.run([train_op, merged, l], feed_dict=feed_dict)
-    print("step %d, loss: %f" % (i, loss))
+    ##print("step %d, loss: %f" % (i, loss))
     train_writer.add_summary(summary, i)
 
   # récupération de w et b
   w_final, b_final = sess.run([W, b])
   print("w final %f, b final %f" % (w_final,b_final))
-"""
+
+  # prédictions	
+  y_pred_np = sess.run(y_pred, feed_dict={x: x_np})
+
+plt.clf()
+plt.xlabel("Y-true")
+plt.ylabel("Y-pred")
+plt.scatter(y_np, y_pred_np)
+plt.show()
