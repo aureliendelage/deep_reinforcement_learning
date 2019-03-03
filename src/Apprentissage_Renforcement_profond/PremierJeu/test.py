3bfaa9a75ecd18a89b7bb86e7ed9a43f1e587318
aurelien <aurelien.delage@telecomnancy.eu>
2019-03-03 20:17:42 +0100

    better butter..

--- src/Apprentissage_Renforcement_profond/PremierJeu/test.py
+++ src/Apprentissage_Renforcement_profond/PremierJeu/test.py
@@ -24,11 +24,12 @@ def AtCenter(x,y):
 		return 1
 
 def Fonction_Valeur(x,y,n,Board,marqued):
+	print("aurelien : ",Fonction_probabilite[3][4][1]);
 	##Afficher_Board(Board,n)
 	print(Marqued)
 	if (Marqued[x][y]):
 		print("marque")
-		exit(0)
+		return Board[x][y]
 	##Board[x][y] = max([Board[x][y]*Fonction_probabilite[x][y][1]+Fonction_Valeur(x+1,y,n+1)])
 	if (x==4 and y==4):
 		Afficher_Board(Board,n)
@@ -56,7 +57,7 @@ def Fonction_Valeur(x,y,n,Board,marqued):
 				print ("b vaut : ",b)
 				Marqued[x][y+1] = 1
 			else:
-				a = Board[x][y+1]
+				b = Board[x][y+1]
 		except:
 			print("je ne peux pas aller en haut, x : ",x," y : ",y)
 		try:
@@ -65,7 +66,7 @@ def Fonction_Valeur(x,y,n,Board,marqued):
 				print ("c vaut : ",c)
 				Marqued[x-1][y] = 1
 			else:
-				a = Board[x-1][y]
+				c = Board[x-1][y]
 		except:
 			print("je ne peux pas aller à gauche, x : ",x," y : ",y)
 		try:
@@ -74,13 +75,15 @@ def Fonction_Valeur(x,y,n,Board,marqued):
 				print ("d vaut : ",d)
 				Marqued[x][y-1] = 1
 			else:
-				a = Board[x][y-1]
+				d = Board[x][y-1]
 		except:
 			print("je ne peux pas aller en bas, x : ",x," y : ",y)
+
+
 		print("a : ",a,"b : ",b," c :",c," d : ",d)
 		print ("max : ",max(a,b,c,d))
 		index = [a,b,c,d].index(max(a,b,c,d))
-		print("le max est en ",index)
+		print("le max est en ",index," au point : ",x," ",y)
 		print("je met à jour le board")
 		Board[x][y] = max(a,b,c,d)
 		Afficher_Board(Board,n)
@@ -93,11 +96,11 @@ Board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,1]]
 
 def Afficher_Board(Board,n):
 	print(" \n \n étape : ",n,"Board : ")
-	print(" | ",Board[4][0]," | ",Board[4][1]," | ",Board[4][2]," | ",Board[4][3]," | ",Board[4][4]," | ")
-	print(" | ",Board[3][0]," | ",Board[3][1]," | ",Board[3][2]," | ",Board[3][3]," | ",Board[3][4]," | ")
-	print(" | ",Board[2][0]," | ",Board[2][1]," | ",Board[2][2]," | ",Board[2][3]," | ",Board[2][4]," | ")
-	print(" | ",Board[1][0]," | ",Board[1][1]," | ",Board[1][2]," | ",Board[1][3]," | ",Board[1][4]," | ")
-	print(" | ",Board[0][0]," | ",Board[0][1]," | ",Board[0][2]," | ",Board[0][3]," | ",Board[0][4]," | ")
+	print(" | ",Board[0][4]," | ",Board[1][4]," | ",Board[2][4]," | ",Board[2][4]," | ",Board[3][4]," | ",Board[4][4])
+	print(" | ",Board[0][3]," | ",Board[1][3]," | ",Board[2][3]," | ",Board[2][3]," | ",Board[3][3]," | ",Board[4][3])
+	print(" | ",Board[0][2]," | ",Board[1][2]," | ",Board[2][2]," | ",Board[2][2]," | ",Board[3][2]," | ",Board[4][2])
+	print(" | ",Board[0][1]," | ",Board[1][1]," | ",Board[2][1]," | ",Board[2][1]," | ",Board[3][1]," | ",Board[4][1])
+	print(" | ",Board[0][0]," | ",Board[1][0]," | ",Board[2][0]," | ",Board[2][0]," | ",Board[3][0]," | ",Board[4][0])
 
 
 '''
@@ -115,11 +118,11 @@ Fonction_probabilite = [[[round(random.random(),2),round(random.random(),2),roun
 					   [[round(random.random(),2),round(random.random(),2),round(random.random(),2),round(random.random(),2)],[round(random.random(),2),round(random.random(),2),round(random.random(),2),round(random.random(),2)],[round(random.random(),2),round(random.random(),2),round(random.random(),2),round(random.random(),2)],[round(random.random(),2),round(random.random(),2),round(random.random(),2),round(random.random(),2)],[round(random.random(),2),round(random.random(),2),round(random.random(),2),round(random.random(),2)]]]
 
 print(" \n \nfonction de probabilité:")
-print(" | ",Fonction_probabilite[0][0]," | ",Fonction_probabilite[0][1]," | ",Fonction_probabilite[0][2]," | ",Fonction_probabilite[0][3]," | ",Fonction_probabilite[0][4]," | ")
-print(" | ",Fonction_probabilite[1][0]," | ",Fonction_probabilite[1][1]," | ",Fonction_probabilite[1][2]," | ",Fonction_probabilite[1][3]," | ",Fonction_probabilite[1][4]," | ")
-print(" | ",Fonction_probabilite[2][0]," | ",Fonction_probabilite[2][1]," | ",Fonction_probabilite[2][2]," | ",Fonction_probabilite[2][3]," | ",Fonction_probabilite[2][4]," | ")
-print(" | ",Fonction_probabilite[3][0]," | ",Fonction_probabilite[3][1]," | ",Fonction_probabilite[3][2]," | ",Fonction_probabilite[3][3]," | ",Fonction_probabilite[3][4]," | ")
-print(" | ",Fonction_probabilite[4][0]," | ",Fonction_probabilite[4][1]," | ",Fonction_probabilite[4][2]," | ",Fonction_probabilite[4][3]," | ",Fonction_probabilite[4][4]," | ")
+print(" | ",Fonction_probabilite[0][4]," | ",Fonction_probabilite[1][4]," | ",Fonction_probabilite[2][4]," | ",Fonction_probabilite[3][4]," | ",Fonction_probabilite[4][4]," | ")
+print(" | ",Fonction_probabilite[0][3]," | ",Fonction_probabilite[1][3]," | ",Fonction_probabilite[2][3]," | ",Fonction_probabilite[3][3]," | ",Fonction_probabilite[4][3]," | ")
+print(" | ",Fonction_probabilite[0][2]," | ",Fonction_probabilite[1][2]," | ",Fonction_probabilite[2][2]," | ",Fonction_probabilite[3][2]," | ",Fonction_probabilite[4][2]," | ")
+print(" | ",Fonction_probabilite[0][1]," | ",Fonction_probabilite[1][1]," | ",Fonction_probabilite[2][1]," | ",Fonction_probabilite[3][1]," | ",Fonction_probabilite[4][1]," | ")
+print(" | ",Fonction_probabilite[0][0]," | ",Fonction_probabilite[1][0]," | ",Fonction_probabilite[2][0]," | ",Fonction_probabilite[3][0]," | ",Fonction_probabilite[4][0]," | ")
 
 Marqued = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 Fonction_Valeur(0,0,0,Board,Marqued)
